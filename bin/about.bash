d324c897dfb976beb5f42570f7a3b0b10e95149c
Thomas Freedman <thomasfreedman@protonmail.com>
2022-03-08 16:21:22 -0600

    Add files via upload
    Commit for Pirate Stick RC1

---
+++ bin/about.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+TITLE='About This Pirate Stick'
+zenity --filename="about.txt" --text-info --title="$TITLE" --width=500 --height=400
+
+
+
--- bin/welcome.bash
+++ bin/welcome.bash
@@ -1,14 +1,13 @@
 #!/bin/bash
-if [ -f welcome.off ]; then exit; fi
+if [ -f ~/.config/.welcome.off ]; then exit; fi
 
 TITLE='Welcome!'
-MSG='The welcome screen is now disabled.'
-zenity --filename="$HOME/bin/welcome.txt" --text-info --title="$TITLE" --width=520 --height=400
-if [ $? -ne 0 ]; then
-  zenity --question --text="Stop showing this at startup?" --width=150 --height=100
-  if [ $? -eq 0 ]; then
-    zenity --title="Disabling Welcome..." --info --text="$MSG" --width=280 --height=100
-    touch welcome.off
+if ! zenity --filename="$HOME/bin/welcome.txt" --text-info \
+       --title="$TITLE" --width=520 --height=400; then
+  if zenity --question --text="Stop showing this at startup?" \
+            --width=150 --height=100; then
+    touch $HOME/.config/.welcome.off
+    notify-send -t 7000 "Pirate Stick Welcome:" "Has been disabled."
     exit
   fi
 fi
